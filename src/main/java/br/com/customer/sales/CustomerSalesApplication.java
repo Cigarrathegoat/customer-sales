8919bc83d73f9076f9061e3bcca84703098a97ad
Silvio Maia <ingles.silvio@gmail.com>
2023-08-16 13:30:48 -0300

    created customer sales project and repository

---
+++ src/main/java/br/com/customer/sales/CustomerSalesApplication.java
@@ -0,0 +1,13 @@
+package br.com.customer.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/customer/sales/CustomerSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.customer.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
